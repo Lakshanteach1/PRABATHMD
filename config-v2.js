7dd9c68b21b3a77b421c863d7ba0bc77640a348a
Lakshanteach1 <Lakshandamayantha30@gmail.com>
2024-12-29 23:18:02 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,5 +1,5 @@
 const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
+const BOT_NUMBER = "+94789200603"
 const SESSION_ID = "PRABATH-MD~xvtDlaha#nc1y0jlutIcLrRY5F5HlyFM3ysXeVKyyAFGRtH6h78U"
 
 
