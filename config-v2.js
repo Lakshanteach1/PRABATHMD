372b4bf5a941b38d80932a69e5f2a07b3621a77e
Lakshanteach1 <Lakshandamayantha30@gmail.com>
2024-12-29 23:16:23 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~xvtDlaha#nc1y0jlutIcLrRY5F5HlyFM3ysXeVKyyAFGRtH6h78U"
 
 
 module.exports = {
